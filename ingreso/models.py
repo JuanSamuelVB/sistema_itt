2946605c006e50ae948160ddf668dca8bda4379d
Juan Samuel Vargas <juan.samuel.vargas@gmail.com>
2017-05-08 17:12:28 -0700

    Add models and initial migration.

--- ingreso/models.py
+++ ingreso/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Visitante(models.Model):
+    apellidos = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+class Acceso(models.Model):
+    profesor = models.ForeignKey('sistema.Profesor', 
+                                 on_delete=models.CASCADE)
+    alumno = models.ForeignKey('sistema.Alumno', 
+                                 on_delete=models.CASCADE)
+    visitante = models.ForeignKey('ingreso.Visitante', 
+                                 on_delete=models.CASCADE)
--- titulacion/models.py
+++ titulacion/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Titulacion(models.Model):
+    proyecto = models.CharField(max_length=50)
+    profesor = models.ForeignKey('sistema.Profesor', 
+                                 on_delete=models.CASCADE)
+    alumno = models.ForeignKey('sistema.Alumno', 
+                                 on_delete=models.CASCADE)
