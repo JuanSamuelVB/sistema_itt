5e31ce4ecff33e53faac1fb07f9979057894673d
Juan Samuel Vargas <juan.samuel.vargas@gmail.com>
2017-05-27 16:33:43 -0700

    Fix adding models to admin site.

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import *
-# Register your models here.
+from .models import Alumno, Profesor
+
 admin.site.register(Alumno)
 admin.site.register(Profesor)
