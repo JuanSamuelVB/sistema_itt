cb47651cf5936772b55c0b168c5dc155a1b8da5b
Juan Samuel Vargas <juan.samuel.vargas@gmail.com>
2017-05-27 17:17:45 -0700

    Remove hardcoded URLs.

--- creditos/urls.py
+++ creditos/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from .views import *
 
+app_name = 'creditos'
 urlpatterns = [
     url(r'^nueva-actividad/$',newActividad.as_view(),name='newActividad'),
     url(r'^reporte/$',reporteActividad.as_view(),name="reporte_actividad")
--- sistema/urls.py
+++ sistema/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from .views import *
 
+app_name = 'sistema'
 urlpatterns = [
 	url(r'^$', Welcome.as_view(), name="home")
 ]
