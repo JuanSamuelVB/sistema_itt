1a08af800eb8143aae03771384f4a94eaae36122
Juan Samuel Vargas <juan.samuel.vargas@gmail.com>
2017-05-28 02:52:20 -0700

    Agrega solicitar actividades.

--- creditos/urls.py
+++ creditos/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = 'creditos'
 urlpatterns = [
     url(r'^nueva-actividad/$',newActividad.as_view(),name='newActividad'),
+    url(r'^solicitar/(?P<pk>\d+)/', solicitar, name='solicitar'),
     url(r'^reporte/$',reporteActividad.as_view(),name="reporte_actividad")
 ]
--- creditos/views.py
+++ creditos/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView,CreateView, ListView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
 
 class newActividad(CreateView):
-	template_name = 'creditos/nueva-actividad.html'
-	model = Actividad
-	fields = '__all__'
-	success_url = reverse_lazy('Welcome_View')
+    template_name = 'creditos/nueva-actividad.html'
+    model = Actividad
+    fields = '__all__'
+    success_url = reverse_lazy('Welcome_View')
 
 class reporteActividad(ListView):
-	template_name='creditos/reporte.html'
-	model = Actividad
-	fields='__all__'
+    template_name='creditos/reporte.html'
+    model = Actividad
+    fields='__all__'
+
+
+def solicitar(request, pk):
+    actividad = get_object_or_404(Actividad, pk=pk)
+
+    if not request.user.alumno in actividad.alumnos.all():
+        actividad.alumnos.add(request.user.alumno)
+
+    return redirect('creditos:reporte_actividad')
